252f52c9391f0501d59cd33e7c9ef59e15d5cd31
abcmehedi5 <abcmehedi5@gmail.com>
2023-07-22 15:23:18 +0600

    collage api create

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const CollageDataq = require("./collage.json");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -9,6 +10,12 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// all api routes
+
+app.get("/collage", (req, res) => {
+  res.send(CollageDataq);
 });
 
 app.listen(port, () => {
